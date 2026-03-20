512705ff08caa9c39b99d89886a91aa4167b5b85
tranhoanggg <tranhoang292004gmail.com>
2026-03-20 15:51:45 +0700

    Add Cart State

--- src/main/java/com/tramthuc/paymentservice/PaymentserviceApplication.java
+++ src/main/java/com/tramthuc/paymentservice/PaymentserviceApplication.java
@@ -1,7 +1,9 @@
 package com.tramthuc.paymentservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaymentserviceApplication {
@@ -10,4 +12,8 @@ public class PaymentserviceApplication {
 		SpringApplication.run(PaymentserviceApplication.class, args);
 	}
 
+	@Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
